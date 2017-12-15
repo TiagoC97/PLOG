8bfc24bdd73d388b62b729fce97c5576b8d291e9
mr_base <tiago79c@gmail.com>
2017-12-15 12:49:22 +0000

    Add files via upload

---
+++ Distance/board.pl
@@ -0,0 +1,150 @@
+% Creates the initial board
+
+initialBoard([
+[@, 3, 5, @],
+[@, @, 2, @],
+[6, 4, 1, 7]]).
+
+% Prints the board
+
+printBoard(Board) :-
+printBlackLine(4),
+printWhiteLine(4),
+printRowByRow(Board),
+printBlackLine(4).
+
+
+printBlackLine(Cols) :-
+write('+--'),
+printBlackLineAux(Cols),
+write('+'),
+nl.
+
+printBlackLineAux(0).
+
+printBlackLineAux(Cols) :-
+write('---------'),
+Cols1 is Cols - 1,
+printBlackLineAux(Cols1).
+
+
+printWhiteLine(Cols) :-
+write('|  '),
+printWhiteLineAux(Cols),
+write('|'),
+nl.
+
+printWhiteLineAux(0).
+
+printWhiteLineAux(Cols) :-
+write('         '),
+Cols1 is Cols - 1,
+printWhiteLineAux(Cols1).
+
+
+
+printRowByRow([]).
+printRowByRow([Line|Rest]) :-
+write('|  '),
+printSingleRowFirst(Line),
+write('|  '),
+printSingleRowSecond(Line),
+write('|  '),
+printSingleRowThird(Line),
+write('|  '),
+printSingleRowFourth(Line),
+write('|  '),
+printSingleRowFifth(Line),
+printWhiteLine(4),
+printRowByRow(Rest).
+
+printSingleRowFirst([Cell]):-
+translateCellFirst(Cell),
+write('  |'),
+nl.
+
+printSingleRowFirst([Cell|More]):-
+translateCellFirst(Cell),
+write('  '),
+printSingleRowFirst(More).
+
+printSingleRowSecond([Cell]):-
+translateCellSecond(Cell),
+write('  |'),
+nl.
+
+printSingleRowSecond([Cell|More]):-
+translateCellSecond(Cell),
+write('  '),
+printSingleRowSecond(More).
+
+printSingleRowThird([Cell]):-
+translateCellThird(Cell),
+write('  |'),
+nl.
+
+printSingleRowThird([Cell|More]):-
+translateCellThird(Cell),
+write('  '),
+printSingleRowThird(More).
+
+printSingleRowFourth([Cell]):-
+translateCellFourth(Cell),
+write('  |'),
+nl.
+
+printSingleRowFourth([Cell|More]):-
+translateCellFourth(Cell),
+write('  '),
+printSingleRowFourth(More).
+
+printSingleRowFifth([Cell]):-
+translateCellFifth(Cell),
+write('  |'),
+nl.
+
+printSingleRowFifth([Cell|More]):-
+translateCellFifth(Cell),
+write('  '),
+printSingleRowFifth(More).
+
+
+translateCellFirst(@) :-
+write('       ').
+
+translateCellFirst(_) :-
+write('+-----+').
+
+
+translateCellSecond(@) :-
+write('       ').
+
+translateCellSecond(_) :-
+write('|     |').
+
+
+translateCellThird(@) :-
+write('       ').
+
+translateCellThird(Cell) :-
+write('| '),
+((Cell < 10, write('00'));
+(Cell > 10, Cell < 100, write('0'));
+(Cell > 100)),
+write(Cell),
+write(' |').
+
+
+translateCellFourth(@) :-
+write('       ').
+
+translateCellFourth(_) :-
+write('|     |').
+
+
+
+translateCellFifth(@) :-
+write('       ').
+
+translateCellFifth(_) :-
+write('+-----+').
